b3479ef34ce34deaace6e134531a3745012ae19a
assekho2 <157326482+assekho2@users.noreply.github.com>
2024-08-24 15:26:24 -0600

    added functionality to buttons

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 from tkinter import *
 
-def go_to_main_page():
-    openingText = Label(root, text = "I am a...", font = ("Times New Roman", 30))
-    openingText.pack()
-
-    studentButton = Button(root, text = 'Student', font = ("Times New Roman", 20), command = lambda: stud_func())
-    studentButton.pack()
-
-    professorButton = Button(root, text = 'Professor', font = ("Times New Roman", 20), command = lambda: prof_func())
-    professorButton.pack()
-
 def clear_frame():
     for widget in frame.winfo_children():
         widget.destroy()
 
-
-def stud_func():
+def go_to_main_page():
     clear_frame()
-    openingText = Label(root, text = "bark bark bark", font = ("Times New Roman", 30))
+    openingText = Label(frame, text = "I am a...", font = ("Times New Roman", 30))
     openingText.pack()
 
+    studentButton = Button(frame, text = 'Student', font = ("Times New Roman", 20), command = lambda: go_to_stud_page())
+    studentButton.pack()
 
-def prof_func():
-    return
+    professorButton = Button(frame, text = 'Professor', font = ("Times New Roman", 20), command = lambda: go_to_prof_page())
+    professorButton.pack()
+
+def go_to_stud_page():
+    clear_frame()
+    
+    
+
+def go_to_prof_page():
+    clear_frame()
+
 
 
 root = Tk()
 frame = Frame(root)
+frame.pack(fill="both", expand = True)
 root.geometry("400x200")
 root.title('Easy UofA Lab Finder')
 
