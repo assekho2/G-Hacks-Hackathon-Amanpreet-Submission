bc40dd46aa9571996714446e9bcc3b1b238256d6
assekho2 <157326482+assekho2@users.noreply.github.com>
2024-08-25 10:40:45 -0600

    variablized prof entries and checks

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSel
 
 db = client.oppurtunitiesdb
 
-opps = db.opps
+opps = db.opps 
 
 def clear_frame():
     for widget in frame.winfo_children():
@@ -40,30 +40,33 @@ def go_to_stud_page():
     backButton.pack()
 
 
-    
+
 def go_to_prof_page():
     clear_frame()
+
+    global profName
+    global profEmail
+    global gradVar
+    global uGradVar
 
     profText = Label(frame, text = 'My full name is...', font=("Times New Roman", 14))
     profText.pack()
 
-    profName = StringVar(value = '')
     nameEntry = Entry(frame, textvariable = profName, font=("Times New Roman", 14))
     nameEntry.pack(pady=10)
 
     profText2 = Label(frame, text = 'My UofA email is...', font=("Times New Roman", 14))
     profText2.pack()
 
-    profEmail = StringVar(value = '')
     emailEntry = Entry(frame, textvariable = profEmail, font=("Times New Roman", 14))
     emailEntry.pack(pady=10)
 
     profText3 = Label(frame, text = 'I am looking for ...', font=("Times New Roman", 14))
     profText3.pack()
 
-    grads = Checkbutton(frame, text = "Grad(s)")
+    grads = Checkbutton(frame, text = "Grad(s)", variable = gradVar, onvalue = 1, offvalue = 0)
     grads.pack()
-    uGrads = Checkbutton(frame,text = "UnderGrad(s)")
+    uGrads = Checkbutton(frame,text = "UnderGrad(s)", variable = uGradVar, onvalue = 1, offvalue = 0)
     uGrads.pack()
 
     nextButton = Button(frame, text = "Next",font = ("Times New Roman", 20), command = lambda: go_to_prof2_page())
@@ -76,23 +79,28 @@ def go_to_prof_page():
 
 def go_to_prof2_page():
     clear_frame()
+    
+    global paidVar
+    global volunteerVar
+    global gResearchVar
+    global iStudyVar
 
     profText = Label(frame, text = 'This is a...', font=("Times New Roman", 20))
     profText.pack()
 
-    paid = Checkbutton(frame, text = "paid")
+    paid = Checkbutton(frame, text = "paid", variable = paidVar, onvalue = 1, offvalue = 0)
     paid.pack()
-    uPaid = Checkbutton(frame,text = "unpaid")
-    uPaid.pack()
+    paidText = Label(frame, text = "(Leave blank for unpaid)")
+    paidText.pack()
 
     profText2 = Label(frame, text = '...', font=("Times New Roman", 20))
     profText2.pack()
 
-    volunteer = Checkbutton(frame, text = "volunteer")
+    volunteer = Checkbutton(frame, text = "volunteer", variable = volunteerVar, onvalue = 1, offvalue = 0)
     volunteer.pack()
-    gradR = Checkbutton(frame,text = "grad research")
+    gradR = Checkbutton(frame,text = "grad research", variable = gResearchVar, onvalue = 1, offvalue = 0)
     gradR.pack()
-    iStudy = Checkbutton(frame, text = "independent study")
+    iStudy = Checkbutton(frame, text = "independent study", variable = iStudyVar, onvalue = 1, offvalue = 0)
     iStudy.pack()
 
     profText3 = Label(frame, text = '...oppurtunity.', font=("Times New Roman", 20))
@@ -107,17 +115,19 @@ def go_to_prof2_page():
     frame.update_idletasks()
 
 
+
 def go_to_prof3_page():
     clear_frame()
 
-    profText = Label(frame, text = 'Finally, add a description for the position', font=("Times New Roman", 20))
+    global desc
+
+    profText = Label(frame, text = 'Finally, add a description for the position', font=("Times New Roman", 20) )
     profText.pack()
 
-    desc = StringVar(value = '')
     descEntry = Entry(frame, textvariable = desc, font=("Times New Roman", 14))
     descEntry.pack(pady=10)
 
-    finishButton = Button(frame, text = "Finish",font = ("Times New Roman", 20))
+    finishButton = Button(frame, text = "Finish",font = ("Times New Roman", 20), command = lambda: pp(profName, profEmail))
     finishButton.pack()
 
     backButton = Button(frame, text = "Back",font = ("Times New Roman", 20), command = lambda: go_to_prof2_page())
@@ -125,13 +135,41 @@ def go_to_prof3_page():
 
     frame.update_idletasks()
 
-
+def pp(name, email):
+    print(name.get())
+    print(email.get())
+    print(desc.get())
+    print(uGradVar.get())
+    print(gradVar.get())
+    print(volunteerVar.get())
+    print(gResearchVar.get())
+    print(iStudyVar.get())
 
 root = Tk()
 frame = Frame(root)
 frame.pack(fill="both", expand = True)
 root.geometry("800x500")
 root.title('Easy UofA Lab Finder')
+
+# All global variables using this library must be declared after root = Tk().
+global profName
+global profEmail 
+global desc
+profName = StringVar()
+profEmail = StringVar()
+desc = StringVar()
+global gradVar
+global uGradVar
+global paidVar
+global volunteerVar
+global gResearchVar
+global iStudyVar
+gradVar = IntVar()
+uGradVar = IntVar()
+paidVar = IntVar() 
+volunteerVar = IntVar()
+gResearchVar = IntVar()
+iStudyVar = IntVar()
 
 go_to_main_page()
 
--- tester.py
+++ tester.py
@@ -1,62 +1,34 @@
-# Program to make a simple 
-# login screen  
-
-
 import tkinter as tk
- 
-root=tk.Tk()
 
-# setting the windows size
-root.geometry("600x400")
- 
-# declaring string variable
-# for storing name and password
-name_var=tk.StringVar()
-passw_var=tk.StringVar()
+# Step 1: Create a global StringVar
 
- 
-# defining a function that will
-# get the name and password and 
-# print them on the screen
-def submit():
 
-    name=name_var.get()
-    password=passw_var.get()
+def update_variable():
+    """
+    This function will be called to demonstrate that the global_string_var
+    can be accessed and used in different functions.
+    """
+    global global_string_var
+    print("Current value of the variable:", global_string_var.get())
+
+def main():
+    # Create the main window
+    root = tk.Tk()
+    root.title("Tkinter StringVar Exa   mple")
+
+    global global_string_var
+    global_string_var = tk.StringVar()
     
-    print("The name is : " + name)
-    print("The password is : " + password)
-    
-    name_var.set("")
-    passw_var.set("")
-    
-    
-# creating a label for 
-# name using widget Label
-name_label = tk.Label(root, text = 'Username', font=('calibre',10, 'bold'))
- 
-# creating a entry for input
-# name using widget Entry
-name_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
- 
-# creating a label for password
-passw_label = tk.Label(root, text = 'Password', font = ('calibre',10,'bold'))
- 
-# creating a entry for password
-passw_entry=tk.Entry(root, textvariable = passw_var, font = ('calibre',10,'normal'), show = '*')
- 
-# creating a button using the widget 
-# Button that will call the submit function 
-sub_btn=tk.Button(root,text = 'Submit', command = submit)
- 
-# placing the label and entry in
-# the required position using grid
-# method
-name_label.grid(row=0,column=0)
-name_entry.grid(row=0,column=1)
-passw_label.grid(row=1,column=0)
-passw_entry.grid(row=1,column=1)
-sub_btn.grid(row=2,column=1)
- 
-# performing an infinite loop 
-# for the window to display
-root.mainloop()
+    # Step 2: Create an Entry widget and link it to the global StringVar
+    entry = tk.Entry(root, textvariable=global_string_var)
+    entry.pack(pady=10)
+
+    # Create a button to trigger the update_variable function
+    button = tk.Button(root, text="Print Variable", command=update_variable)
+    button.pack(pady=10)
+
+    # Start the Tkinter event loop
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
